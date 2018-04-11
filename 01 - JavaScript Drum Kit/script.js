2e26739af2fd708c3115081b02234ffa07372e7a
Mario <mariob360@gmail.com>
2018-04-11 15:34:59 -0700

    Checks for audio and plays if found

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,7 +1,9 @@
 window.addEventListener('keydown', function(e) {
-  console.log(e.which);
 
   //Check for corrosponding audio element in html file
   const audio = document.querySelector(`audio[data-key="${e.which}"]`);
-  console.log(audio);
+  if(!audio) return;
+
+  audio.play();
+
 })
