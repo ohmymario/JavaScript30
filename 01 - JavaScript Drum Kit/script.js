78e851be1716348806194984515252a0f55f3864
Mario <mariob360@gmail.com>
2018-04-11 23:48:40 -0700

    Add values for audio and add class when keypress

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -2,7 +2,14 @@ window.addEventListener('keydown', function(e) {
 
   //Check for corrosponding audio element in html file
   const audio = document.querySelector(`audio[data-key="${e.which}"]`);
+  const key = document.querySelector(`.key[data-key="${e.which}"]`);
+
   if(!audio) return;
+
+  key.classList.add('playing');
+
+  audio.currentTime = 0; //Restart audio if key hit again
+  audio.volume = 0.6; //lower volume
 
   audio.play();
 
