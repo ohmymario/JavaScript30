2b6a0ff749a510a3c22d41d1809539e7aac51c20
Mario <mariob360@gmail.com>
2018-04-15 15:12:00 -0700

    Add function to update css based on input

--- 03 - CSS Variables/scripts.js
+++ 03 - CSS Variables/scripts.js
@@ -2,5 +2,13 @@ const inputs = document.querySelectorAll('.controls input');
 console.log(inputs);
 
 function handleUpdate() {
+  const suffix = this.dataset.sizing || '' ; // access "data-" attribute if present (px)
+
+  //this.value will be pixel size or color depeding on which input modifying
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
 }
+
+// Will continually update whether with just kb or mouse input
+inputs.forEach(input => input.addEventListener('change', handleUpdate))
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
 
