0158b5f36fefa3e7167e2ad049d1eae6dbc9ea14
Mario <mariob360@gmail.com>
2018-04-16 15:45:37 -0700

    Add transition when previous transition ends

--- 05 - Flex Panel Gallery/scripts.js
+++ 05 - Flex Panel Gallery/scripts.js
@@ -5,7 +5,15 @@ function toggleOpen() {
   this.classList.toggle('open');
 }
 
+function toggleActive(event) {
+  // Safari reconizes the ending transition as flex vs flex-grow
+  if(event.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
 panels.forEach(panel => {
   console.log(panel);
   panel.addEventListener('click', toggleOpen);
+  panel.addEventListener('transitionend', toggleActive);
 });
