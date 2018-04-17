b236472119a13ff95842de2e9af8c7fbdc98940c
Mario <mariob360@gmail.com>
2018-04-16 18:21:00 -0700

    Add displayMatches function

--- 06 - Type Ahead/scripts.js
+++ 06 - Type Ahead/scripts.js
@@ -1,5 +1,8 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
 const cities = [];
 
 fetch(endpoint)
@@ -15,3 +18,19 @@ function findMatches(wordToMatch, cities) {
     return place.city.match(regex) || place.state.match(regex);
   }) 
 }
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray.map(place => {
+    return`
+      <li>
+        <span class="name">${place.city}, ${place.state}</span>
+        <span class="population">${place.population}</span>
+      </li>
+    `
+  }).join(''); // To create on big string
+  suggestions.innerHTML = html;
+}
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
