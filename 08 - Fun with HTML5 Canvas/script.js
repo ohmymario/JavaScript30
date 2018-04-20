32a7da4f452720f250a1a01620649837c2839188
Mario <mariob360@gmail.com>
2018-04-19 18:29:27 -0700

    Mouse is now only logged when mousedown

--- 08 - Fun with HTML5 Canvas/script.js
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -7,3 +7,19 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55'; // color/gradient/patter
 ctx.lineJoin = 'round'; //type of corner when two lines meet
 ctx.lineCap = 'round'; // style of the end of line
+
+let isDrawing = false; // Flag to determine if drawing to canvas
+let lastX = 0; // Begin of line
+let lastY = 0; // End of line
+
+function draw(e) {
+  console.log('mousemove is working');
+  if (!isDrawing) return; // if isDrawing is false|stop the function
+
+  console.log(e);
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
