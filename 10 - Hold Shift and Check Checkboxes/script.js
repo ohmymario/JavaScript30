6d91ab49a66537056983ea959d4847da28f7b6f0
Mario <mariob360@gmail.com>
2018-04-22 16:24:59 -0700

    Create handleCheck

--- 10 - Hold Shift and Check Checkboxes/script.js
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -1,14 +1,21 @@
 // shift = 16 keycode keydown
-
 const checkboxes = document.querySelectorAll('.item>input[type=checkbox]');
+
+let lastChecked;
+
+function handleCheck(e) {
+  if(e.shiftKey === true) {
+    console.log("TRUEE")
+  }
+
+  console.log(this.checked)
+
+  lastChecked = this;
+}
 
 // Click event fires even if you use your keyboard
 checkboxes.forEach(function(checkbox) {
-  checkbox.addEventListener('click', function() {
-    console.log(this.checked);
-  })
+  checkbox.addEventListener('click', handleCheck)
 })
 
-window.addEventListener('keydown', function(e) {
-  console.log(e.shiftKey)
-})
+
