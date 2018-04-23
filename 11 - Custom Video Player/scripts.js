a83c28767e33a6d1de85b07f5aebdd6fc26585d5
Mario <mariob360@gmail.com>
2018-04-22 20:35:50 -0700

    Selected elements and create togglePlay function

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,20 @@
+// Get Elements
+const player = document.querySelector('.player');
+
+const video = player.querySelector('.viewer')
+const progress = player.querySelector('.progress')
+const progessBar = player.querySelector('.progress__filled')
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// Build out functions
+function togglePlay() {
+  if(video.paused) {
+    video.play()
+  } else {
+    video.pause()
+  }
+}
+
+// Add event listeners
