52fa17429624f5fd094594ab9763828e20e150e4
Mario <mariob360@gmail.com>
2018-04-23 19:33:07 -0700

    Add key sequece detection

--- 12 - Key Sequence Detection/script.js
+++ 12 - Key Sequence Detection/script.js
@@ -1,3 +1,18 @@
+const pressed = [];
+const secretCode = 'mario'
+
 window.addEventListener('keyup', (e) => {
   console.log(e.key);
+  pressed.push(e.key);
+  console.log(pressed);
+  var amountToRemove = pressed.length - secretCode.length;
+
+  pressed.splice(-secretCode.length - 1, amountToRemove);
+
+  if(pressed.join('').includes(secretCode)) {
+    console.log('You found the secret code')
+    cornify_add();
+  }
 })
+
+console.log(pressed);
