801058258ee874e0ff976b528c076798d5bea026
Mario <mariob360@gmail.com>
2018-04-23 19:09:03 -0700

    Add scrub function with event listeners

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -41,6 +41,12 @@ function handleProgress() {
   progessBar.style.flexBasis = `${percent}%`
 }
 
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+
+  video.currentTime = scrubTime;
+}
+
 // Add event listeners
 video.addEventListener('click', togglePlay)
 toggle.addEventListener('click', togglePlay)
@@ -55,5 +61,8 @@ skipButtons.forEach(button => button.addEventListener('click', skip))
 ranges.forEach(range => range.addEventListener('click', handleRangeUpdate))
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate))
 
-
-
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
