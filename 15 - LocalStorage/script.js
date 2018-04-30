ac36227b1f66bfe13eb33a7b88cff8ea33bf3b23
Mario <mariob360@gmail.com>
2018-04-29 19:55:31 -0700

    Add comments and checkboxes

--- 15 - LocalStorage/script.js
+++ 15 - LocalStorage/script.js
@@ -1,33 +1,35 @@
-
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
 
 function addItem(e) {
   e.preventDefault(); // Prevent Refresh
+
   const text = this.querySelector('[name=item]').value; // refers to clicked submit buttons inputted value
-  console.log(text)
   const item = {
     text, 
     done: false
   }
 
-  console.log(item);
-  this.reset(); // Clears form for next input
-
   items.push(item);
   populateList(items, itemsList)
+  this.reset(); // Clears form for next input
+
 }
 
 // default of empty array to prevent errors
 function populateList(plates = [], platesList) {
+
+  // Create HTML | ${i} will reflect the index number
   platesList.innerHTML = plates.map((plate, i) => {
     return `
     <li>
-      <label for="">${plate.text}</label>
+      <input type="checkbox" data-index="${i}" id="item${i}" checked>
+      <label for="item${i}">${plate.text}</label>
     </li>
     `;
   }).join('');
+
 }
 
 addItems.addEventListener('submit', addItem) 
