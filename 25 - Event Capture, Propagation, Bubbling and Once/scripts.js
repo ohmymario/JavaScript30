434ca58742565b6b9d34cb18a397a2d579da07d2
Mario <mariob360@gmail.com>
2018-09-11 20:43:38 -0700

    recap on bubbling

---
+++ 25 - Event Capture, Propagation, Bubbling and Once/scripts.js
@@ -0,0 +1,22 @@
+const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
+
+function logText(e) {
+  console.log(this.classList.value);
+  // e.stopPropagation(); // Stop bubbling
+}
+
+divs.forEach(div =>
+  div.addEventListener('click', logText, {
+    capture: false,
+    once: true,
+  })
+);
+
+button.addEventListener(
+  'click',
+  () => {
+    console.log('click!!!!!');
+  },
+  { once: true }
+);
